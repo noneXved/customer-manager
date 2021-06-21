def93780d99e2b0526dc1b98b85c74d579f4597b
noneXved <bajno.damian96@gmail.com>
2021-06-21 22:39:25 +0200

    Template variables and model data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
+import {Customer} from './model';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1 [style.color]="nameColor" [class.isActive]="isActive">{{ name.toUpperCase() }}</h1>
-    <img [src]="photoUrl" height="200" width="200"/>
+    <p>{{nameInput.value}}</p>
+    <h1 [style.color]="nameColor" [class.isActive]="isActive">{{ customer.name.toUpperCase() }}</h1>
+    <p>Opis: {{ customer.description }}</p>
+    <p>Wiek: {{ customer.age }}</p>
+    <p>Adres: {{ customer.address.city }} {{customer.address.street}} {{ customer.address.houseNumber }}</p>
+    <img [src]="customer.photoUrl" height="200" width="200"/>
     <br/>
     <button (click)="changeIsActive()" type="button">Przełącz podkreślenie</button>
     <br/>
     <button (click)="changeColor()" type="button">Przełącz kolor</button>
+    <br/>
+    <input type="text" [(ngModel)]="customer.name" [ngModelOptions]="{updateOn: 'blur'}"/>
+<!--    Zmienne szablonowe maja zasięg w całym szablonie-->
+    <input #nameInput type="text" [value]="customer.name" (input)="customer.name = nameInput.value" />
     `,
   styles: [
       '.isActive { text-decoration: underline; }'
   ]
 })
 export class AppComponent {
-  name: string = "Jan Kowalski";
+  customer: Customer = {
+    name: "Jan Kowalski",
+    photoUrl: "assets/images/wallpaperzipper.jpg",
+    age: 0,
+    description: "a",
+    address: {
+      city: "Warsaw",
+      street: "Dmowski",
+      houseNumber: 3
+    }
+  };
+
   nameColor: string = "blue";
-  photoUrl: string = "assets/images/wallpaperzipper.jpg";
   isActive: boolean = true;
 
 
