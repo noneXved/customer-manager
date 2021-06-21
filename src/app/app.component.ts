b59d29f5c26918cb070629fb7e58700c370c3491
noneXved <bajno.damian96@gmail.com>
2021-06-21 19:02:37 +0200

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{ name.toUpperCase() }}</h1>
+    <h1 [style.color]="nameColor" [class.isActive]="isActive">{{ name.toUpperCase() }}</h1>
     <img [src]="photoUrl"/>
     `,
-  styles: []
+  styles: [
+      '.isActive { text-decoration: underline; }'
+  ]
 })
 export class AppComponent {
   name: string = "Jan Kowalski";
+  nameColor: string = "blue";
   photoUrl: string = "assets/images/wallpaperzipper.jpg";
+  isActive: boolean = true;
+
+
   constructor() {
-    setTimeout(() =>{
-      this.name = "Michał";
-    }, 3000);
   }
 
 }
